5c9b9453a187c6eb05072c9580630451564046da
root <root@t-shinkai00>
2021-06-09 03:56:37 +0900

    update

--- slack_post_image.py
+++ slack_post_image.py
@@ -3,8 +3,8 @@ from variables import TOKEN,CHANNEL
 files = {'file': open("/root/sensor_test/images/2021-06-09 03:09:05.292639.jpg", 'rb')}  #****に送付ファイル名記載。絶対パスで記載が安全。 
 
 param = {
-     'token':"36WPIHl1lubZlABuyRyGdb3F",
-     'channels':"C024DRUKXGS",
+     'token':TOKEN,
+     'channels':CHANNEL,
     #  'filename':"DLファイル名",　 #.txtとかファイル名を付ける
      'initial_comment': "動きを検知しました。",
      'title': "様子"
